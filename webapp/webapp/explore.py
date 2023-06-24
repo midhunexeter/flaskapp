d1f0dda9167cf71c800826a4dac882e53356eaa1
midhuniitm <midhungecster@gmail.com>
2023-06-24 18:40:46 +0100

    refactoring

--- webapp/webapp/explore.py
+++ webapp/webapp/explore.py
@@ -1,3 +1,19 @@
 import pandas as pd
+from numpy import random
+import chess
+def play_game():
+    board = chess.Board()
+    random.random
+    for i in range(1000):
+        moves = [mv.uci() for mv in list(board.generate_legal_moves())]
+        movindex =random.randint(0, len(moves))
+        mov = moves[movindex]
+        board.push_uci(mov)
+        if board.is_game_over():
+            board
+            break
+    return board
 
-df = pd.read_csv('data/games.csv')
+
+
+df = pd.read_csv('../data/games.csv')
