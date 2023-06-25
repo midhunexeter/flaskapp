444a32c2560b188559da9b269b0f357ec3a52b50
midhuniitm <midhungecster@gmail.com>
2023-06-25 14:01:06 +0100

    refactored getmove

--- webapp/webapp/explore.py
+++ webapp/webapp/explore.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from numpy import random
 import chess
+
+
+def get_move(board):
+    moves = [mv.uci() for mv in list(board.generate_legal_moves())]
+    movindex =random.randint(0, len(moves))
+    mov = moves[movindex]
+    return mov
+
+
 def play_game():
     board = chess.Board()
     random.random
     for i in range(1000):
-        moves = [mv.uci() for mv in list(board.generate_legal_moves())]
-        movindex =random.randint(0, len(moves))
-        mov = moves[movindex]
+        mov = get_move(board)
         board.push_uci(mov)
         if board.is_game_over():
             board
