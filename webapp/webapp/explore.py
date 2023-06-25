3af169c067f6cb5ef68804c2c4787a4f770357f9
midhuniitm <midhungecster@gmail.com>
2023-06-25 16:43:39 +0100

    all possible moves upto 4

--- webapp/webapp/explore.py
+++ webapp/webapp/explore.py
@@ -1,14 +1,42 @@
 import pandas as pd
 from numpy import random
 import chess
-
-
+from copy import deepcopy
+import itertools
 def get_move(board):
-    moves = [mv.uci() for mv in list(board.generate_legal_moves())]
+    moves = get_move_list_for_board(board)
     movindex =random.randint(0, len(moves))
     mov = moves[movindex]
     return mov
 
+def get_move_list_for_board(board):
+    moves = [mv.uci() for mv in list(board.generate_legal_moves())]
+    return moves
+
+def get_board_list_for_board(board):
+    moves = [mv.uci() for mv in list(board.generate_legal_moves())]
+    boards = [make_a_move(board, mv) for mv in moves]
+    return boards
+
+def get_board_list_for_boards(boards):
+    board_list= []
+    for board in boards:
+        board_list.append(get_board_list_for_board(board))
+    return itertools.chain(*board_list)
+
+def nestlist(f,x,c):
+    # Mathematica's nestlist reproduced
+    result = [x]
+    for i in range(c):
+        x = f(x)
+        result.append(x)
+    return result
+
+
+def make_a_move(board, move):
+    board_copy = deepcopy(board)
+    board_copy.push_uci(move)
+    return board_copy
 
 def play_game():
     board = chess.Board()
@@ -21,6 +49,4 @@ def play_game():
             break
     return board
 
-
-
-df = pd.read_csv('../data/games.csv')
+df = pd.read_csv("data/games.csv")
